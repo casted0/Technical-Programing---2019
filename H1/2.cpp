caf8b94ba454de94d236e909797ab9c905867cf1
Kasteo <pablo.castedo@estudiante.uam.es>
2019-09-11 12:11:22 +0200

    H1 Final, with extra point

--- H1/2.cpp
+++ H1/2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#define NOT_PRIME -1
 using namespace std;
 
 // Find all prime numbers untill a number n given by the user
@@ -8,25 +9,21 @@ using namespace std;
 // Calculate also the sum of reciprocals of prime numbers
 // less than n and reciprocals of twin primes less than n:
 
-bool PrimeNumber(int N, vector<int> primes){
+// V2 - Sieve of Eratosthenes: 
+// 1 - Put all the numbers in a vector
+// 2 - Sieve all the multiples of each number untill the next prime number found squared is bigger than N
 
-    for(vector<int>::size_type i = 0; i < primes.size(); i++){
-
-        if(primes[i] >= 49){
-            break;
-        }else if(!(N % primes[i])){
-            return false;
-        }
-    }
-
-    return true;
-
+int pow(int n){
+    return n*n;
 }
 
 int main(){
 
     vector<int> primes;
     int max_n;
+    int prime_checker;
+    int checked;
+    long normal_sum = 0;
     double reciprocal_sum = 0.0;
     double reciprocal_sum_twin = 0.0;
 
@@ -37,27 +34,66 @@ int main(){
 
     }while(max_n < 3);
 
-    primes.push_back(2);
-    int i = 3;
+    for(vector<int>::size_type i = 2; i < max_n; i++){
 
-    while(i < max_n){
+        primes.push_back(i);
+    }
 
-        if((i % 2) && (i % 3) && (i % 5) && (i % 6)){   
-            i++;
-        }else if(PrimeNumber(i, primes)){
-            primes.push_back(i);
-            i++;
+
+    for(vector<int>::size_type i = 0; pow(prime_checker) < max_n; i++){
+
+        if(primes.at(i) != NOT_PRIME){
+
+            prime_checker = primes.at(i);
+            cout << "Prime Checker: " << prime_checker << endl;
+
+            for(vector<int>::size_type j = i+1; j < primes.size(); j++){
+
+                checked = primes.at(j);
+            
+                if(checked != NOT_PRIME){
+
+                    if(!(checked % prime_checker)){
+                        primes[j] = NOT_PRIME;
+                    }
+                }
+            }
         }
 
     }
 
-    for(vector<int>::size_type i = 0; i < primes.size(); i++){
-        cout << "Prime[" << i+1 << "]: " << primes[i] << endl;
+    for(vector<int>::size_type i = 0; i < primes.size()-1; i++){
+
+        if(primes[i] != NOT_PRIME){
+
+            cout << "Prime: " << primes[i] << endl;
+        }
     }
+
+    for(vector<int>::size_type i = 0; i < primes.size(); i++){
+
+        if(primes[i] != NOT_PRIME){
+
+            reciprocal_sum += (1/(double)primes[i]);
+            normal_sum ++;
+        }
+        
+    }
+
+    for(vector<int>::size_type i = 0, j = 2; i < primes.size(); i++, j++){
+
+        if(primes[i] != NOT_PRIME && primes[j] != NOT_PRIME){
+            reciprocal_sum_twin += (1/(double)primes[i]) + (1/(double)primes[j]);
+        }
+        
+    }
+
+    cout << "Prime numbers amount: " << normal_sum << endl;
+    cout << "Reciprocal sum: " << reciprocal_sum << endl;
+    cout << "Reciprocal sum of twin primes: " << reciprocal_sum_twin << endl;
+
 
     return 0;
 
 }
 
-// Fastest way to see if a number n is prime:
-// Check if that number is divisible by any of the prime numbers that precede n square root
